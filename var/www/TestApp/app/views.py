295d8530b2bae25e2f17bf7b1e0b3ce26fd7d06f
Ferdi Bulus <ferdi_bulus@hotmail.com>
2023-03-24 17:11:10 +0300

    commit 1

--- var/www/TestApp/app/views.py
+++ var/www/TestApp/app/views.py
@@ -1,9 +1,11 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 def home():
-    return "Hello world!"
+    return "Hello worlddfasdas!"
+
 
 @app.route('/template')
 def template():
--- var/www/TestApp/main.py
+++ var/www/TestApp/main.py
@@ -1,1 +1,10 @@
 from app import app
+from flask import render_template
+@app.route('/')
+def home():
+    return "Hello world!asddasda"
+
+
+@app.route('/template')
+def template():
+    return render_template('home.html')
