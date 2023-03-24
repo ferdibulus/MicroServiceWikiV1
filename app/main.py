2d4fa2bfe12f588c80f2398864aa73018f7dcaea
Ferdi Bulus <ferdi_bulus@hotmail.com>
2023-03-24 20:25:44 +0300

    commit 1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
 from flask import Flask
+import json
+import event
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+# Start taking data from api in compile time
+print("Initial Data process has been started. It'll take approximately 15 minutes")
+#event.getDataFromApi()
+
+
+@app.route("/api/getAllEvents")
+def get():
+    return json.dumps([obj.__dict__ for obj in event.returnData])
+
+@app.route("/api/deneme")
+def getaa():
+    return "Hello Ferdi"
 
 @app.route("/")
 def hello():
