fbea81bdcf3806a3f1e850be2497abf6c950bd8e
Ferdi Bulus <ferdi_bulus@hotmail.com>
2023-03-24 17:30:44 +0300

    commit 1

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from app import app
+from flask import Flask
+app = Flask(__name__)
+
 from flask import render_template
 @app.route('/')
 def home():
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from www.TestApp.app import app
+from flask import render_template
+@app.route('/')
+def home():
+    return "Hello world!asddasda"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route('/template')
+def template():
+    return render_template('home.html')
