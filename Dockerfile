e606d3fc06ae5ef37bdd770732699c1e35903ed4
Ferdi Bulus <ferdi_bulus@hotmail.com>
2023-03-24 20:36:15 +0300

    commit 1

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.8
 
 COPY app /app
+RUN pip install Flask \
+    && pip install APScheduler \
+    && pip install -U flask-cors \
+    && pip install wikipedia
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import json
-import event
+#import event
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,16 +8,20 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Start taking data from api in compile time
 print("Initial Data process has been started. It'll take approximately 15 minutes")
-#event.getDataFromApi()
+
+
+# event.getDataFromApi()
 
 
 @app.route("/api/getAllEvents")
 def get():
     return json.dumps([obj.__dict__ for obj in event.returnData])
 
+
 @app.route("/api/deneme")
 def getaa():
     return "Hello Ferdi"
+
 
 @app.route("/")
 def hello():
